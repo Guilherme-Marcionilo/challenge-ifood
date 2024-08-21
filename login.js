94b624172b3b085dc07da68241c86d00e4b142d7
GuiMenoti <dorguis05@gmail.com>
2024-08-21 19:53:11 -0300

    Add topnav

--- login.js
+++ login.js
@@ -1,11 +1,13 @@
 
 function validacao() {
 
-    let email = "login@login.com"
-    let resultadoEmail = document.getElementById('email-form').value;
+let email = "login@login.com";
+let resultadoEmail = document.getElementById('email-form').value;
 
   if( resultadoEmail == email) {
     document.getElementById('message').innerHTML = "<p class='text-success'>Login bem-sucedido!</p>";
+    window.location.href = "sidebar.html";
+    
     
   } else {
     document.getElementById('message').innerHTML = "<p class='text-danger'>E-mail não encontrado.</p>";
