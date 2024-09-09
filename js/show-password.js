00fbddb7b380b211b47ce6bda9db1e2b56733818
GuiMenoti <dorguis05@gmail.com>
2024-09-09 20:26:49 -0300

    ver senha

--- js/show-password.js
+++ js/show-password.js
@@ -10,3 +10,19 @@ function mostrarSenha(){
     btnShowPass.classList.replace('bi-eye', 'bi-eye-slash')
   }
 }
+
+
+document.getElementById('togglePassword').addEventListener('click', function () {
+  const passwordField = document.getElementById('password');
+  const icon = this;
+
+  if (passwordField.type === 'password') {
+      passwordField.type = 'text';
+      icon.classList.remove('bi-eye-slash');
+      icon.classList.add('bi-eye');
+  } else {
+      passwordField.type = 'password';
+      icon.classList.remove('bi-eye');
+      icon.classList.add('bi-eye-slash');
+  }
+});
